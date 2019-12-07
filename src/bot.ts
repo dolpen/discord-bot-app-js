1afeaaff03e70c4bc9a0342f170840d4bcaf83f2
dolpen <dolpen@dolpen.net>
2019-12-07 13:12:24 +0900

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -160,7 +160,7 @@ operationHandler.addHandler('play', (message, command) => {
         .then(([url, channel]) => {
             channel.join()
                 .then((connection) => {
-                    const dl = ytdl(url , { filter: 'audio'})
+                    const dl = ytdl(url)
                         .on('error', (error) => {
                             debug(error.message)
                         })
